2148f20567d5acddf4b025c134c2efd44942c240
NorthernFaux <shannon.smith17@dcmail.ca>
2025-08-01 20:40:22 -0400

    setup
    added the previous p3 files because I prefer using my own

---
+++ assignment4/p4/main.js
@@ -0,0 +1,120 @@
+// part of p3.html
+
+// setup canvas
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+// function to generate random number
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// function to generate random color
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+
+// ball object
+
+class Ball {
+  // sets position, how fast it is, color and its size
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+
+    // draw the ball on the screen
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+
+    // updates the ball's position
+    update() {
+        // checks if the ball reached the screen edges and updates the trajectory
+        if (this.x + this.size >= width) {
+            this.velX = -this.velX;
+        }
+        
+        if (this.x - this.size <= 0) {
+            this.velX = -this.velX;
+        }
+        
+        if (this.y + this.size >= height) {
+            this.velY = -this.velY;
+        }
+
+        if (this.y - this.size <= 0) {
+            this.velY = -this.velY;
+        }
+
+        this.x += this.velX;
+        this.y += this.velY;
+    }
+
+    // detects if another ball collided with it, then changes its color
+    collisionDetect() {
+        for (const ball of balls) {
+            // ensures the ball being checked is not itself
+            if (this !== ball) {
+                const dx = this.x - ball.x;
+                const dy = this.y - ball.y;
+                const distance = Math.sqrt(dx * dx + dy * dy);
+
+                if (distance < this.size + ball.size) {
+                    ball.color = this.color = randomRGB();
+                }
+            }
+        }
+    }
+}
+
+// holds all ball objects
+const balls = [];
+
+// creates 25 ball objects
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size,
+  );
+
+  balls.push(ball);
+}
+
+// main loop
+function loop() {
+  // fades the previous screen image
+  ctx.fillStyle = "rgb(0 0 0 / 25%)";
+  ctx.fillRect(0, 0, width, height);
+
+  // draws the balls and updates their position; does collision detection
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+    ball.collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop();
