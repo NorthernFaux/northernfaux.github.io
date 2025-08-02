4adc8ef857428438d9d983b7eed9f3c0f570ac43
NorthernFaux <shannon.smith17@dcmail.ca>
2025-08-01 20:10:47 -0400

    image array
    adds an image array with both the image source and alt text

--- assignment4/p2/main.js
+++ assignment4/p2/main.js
@@ -6,9 +6,8 @@ const thumbBar = document.querySelector('.thumb-bar');
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-/* Declaring the array of image filenames */
-
-/* Declaring the alternative text for each image file */
+/* Declaring the array of image filenames and alternative text */
+const imageInfo = [["pic1.jpg", "A butterfly on a leaf"], ["pic2.jpg", "Closeup of a human eye"], ["pic3.jpg", "Rock that looks like a wave"], ["pic4.jpg", "Purple and white flowers"], ["pic5.jpg", "Ancient Egyptian artwork in a tomb"]]
 
 /* Looping through images */
 
